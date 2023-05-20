cbac6e2bb0c5d705253fe09ac899557dad306088
luann <luann00@aipim.io>
2023-05-20 19:38:28 +0900

    feat : 테이블 컬럼 연관관계 메소드 설정

--- src/main/java/jpashop/jpashop/entity/Category.java
+++ src/main/java/jpashop/jpashop/entity/Category.java
@@ -9,6 +9,8 @@ import org.hibernate.annotations.Comment;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Table(name = "category")
 @Getter @Setter
@@ -26,7 +28,16 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     @Comment("상위 카테고리 아이디")
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
