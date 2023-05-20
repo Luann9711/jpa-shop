c7d0463bc480004f0b08bb7ab69201d3b0bee88d
luann <luann00@aipim.io>
2023-05-20 20:59:53 +0900

    feat : memberRepository 개발

---
+++ src/main/java/repo/MemberRepository.java
@@ -0,0 +1,9 @@
+package repo;
+
+import jpashop.jpashop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
